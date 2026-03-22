bc35db810178a7a4207d6cce0e572394f9f558dc
nobody_on_earth <reddeadarthurfan1889@gmail.com>
2026-03-22 09:52:01 -0500

    Update move.c

--- move.c
+++ move.c
@@ -1,11 +1,18 @@
+/*
+Ported to C++ by dercode-solutions-2025
+March 21, 2026
+*/
 #include <stdio.h>
+#include <iostream>
+#include <cstdlib>
 int main(int argc, char** argv) {
     if (argc != 3) {
-        printf("Usage: move <file> <destination>\n");
+        std::cout << "Usage: move <file> <destination>" << "\n";
         return 1;
     }
     if (rename(argv[1], argv[2]) != 0) {
-        perror("FATAL ERROR");
+        perror("[ERROR]: Could not move file.");
+        std::exit(1);
         return 2;
     }
     return 0;
