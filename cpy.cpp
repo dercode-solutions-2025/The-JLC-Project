b1603476951a3341c6e3d5a3d8c73016a393b357
nobody_on_earth <reddeadarthurfan1889@gmail.com>
2026-03-22 09:28:39 -0500

    Update and rename cpy.c to cpy.cpp

--- cpy.cpp
+++ cpy.cpp
@@ -1,25 +1,34 @@
+/*
+Ported to C++ by dercode-solutions-2025
+March 21, 2026
+*/
 #include <stdio.h>
 #include <stdlib.h>
+#include <iostream>
+#include <cstdlib>
 #define BUFFSIZE 1024
 int main(int argc, char** argv) {
     if (argc != 3) {
-        printf("Usage: cpy <source> <destination>\n");
+        std::cout << "Usage: cpy <source> <destination>" << "\n";
         return 1;
     }
     FILE *source = fopen(argv[1], "rb");
     FILE *destination = fopen(argv[2], "wb");
     if (source == NULL) {
-        perror("FATAL ERROR");
+        perror("[ERROR]: Source does not exist.\n");
+        std::exit(1);
         return 2;
     }
     if (destination == NULL) {
-        perror("FATAL ERROR");
+        perror("[ERROR]: Destination does not exist.\n");
+        std::exit(1);
         return 2;
     }
     char buff[BUFFSIZE];
     while (fread(buff, 1, BUFFSIZE, source) > 0) {
         if (fwrite(buff, 1, BUFFSIZE, destination) < 0) {
-            perror("FATAL ERROR");
+            perror("[ERROR]: fwrite() failed.\n");
+            std::exit(1);
             return 3;
         }
     }
