4f23c20a7e6f96f479842f058cb12a8b7893feb6
nobody_on_earth <reddeadarthurfan1889@gmail.com>
2026-03-22 09:33:57 -0500

    Update and rename hn.c to hn.cpp

--- hn.cpp
+++ hn.cpp
@@ -1,10 +1,15 @@
+/*
+Ported to C++ by dercode-solutions-2025
+March 21, 2026
+*/
 #include <stdio.h>
 #include <string.h>
+#include <cstdlib>
 #define BUFFSIZE 1024
 int main(int argc, char** argv) {
     if (argc > 2) {
-        printf("Usage: hn\n");
-        printf("Usage: hn <name>\n");
+        std::cout << "Usage: hn\n" << "\n";
+        std::cout << "Usage: hn <name>\n" << "\n";
         return 1;
     }
     char buf[BUFFSIZE];
@@ -12,18 +17,20 @@ int main(int argc, char** argv) {
     if (argc == 2) {
         f = fopen("/etc/hostname", "r");
         if (f == NULL) {
-            perror("FATAL ERROR");
+            perror("[ERROR]: There was a problem opening the hostname.");
+            std::exit(1);
             return 2;
         }
         while (fgets(buf, BUFFSIZE, f) != NULL) {
-            printf("%s", buf);
+            std::cout << buf << "\n";
         }
         fclose(f);
         return 0;
     }
     f = fopen("/etc/hostname", "w");
     if (f == NULL) {
-        perror("FATAL ERROR");
+        perror("[ERROR]: There was a problem opening the hostname.");
+        std::exit(1);
         return 2;
     }
     fprintf(f, "%s", argv[1]);
