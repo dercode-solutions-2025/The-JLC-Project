232621646471822d57af650bd33758ac40d946e1
nobody_on_earth <reddeadarthurfan1889@gmail.com>
2026-03-22 09:47:01 -0500

    Update and rename mat.c to mat.cpp

--- mat.cpp
+++ mat.cpp
@@ -1,19 +1,26 @@
+/*
+Ported to C++ by dercode-solutions-2025
+March 21, 2026
+*/
 #include <stdio.h>
 #include <stdlib.h>
+#include <iostream>
+#include <cstdlib>
 #define BUFFSIZE 1024
 int main(int argc, char** argv) {
     if (argc != 2) {
-        printf("Usage: mat <file>\n");
+        std::cout << "Usage: mat <file>" << "\n";
         return 1;
     }
     FILE *f = fopen(argv[1], "r");
     if (f == NULL) {
-        perror("FATAL ERROR");
+        perror("[ERROR]: File is empty.");
+        std::exit(1);
         return 2;
     }
     char buff[BUFFSIZE];
     while (fgets(buff, BUFFSIZE, f) != NULL) {
-        printf("%s", buff);
+        std::cout << buff << "\n";
     }
     fclose(f);
     return 0;
